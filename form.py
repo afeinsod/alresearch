7303a916f550e18d541863cc2a62ad3628880938
alfeinsod <alfeinsod@gmail.com>
2014-11-07 16:57:28 -0800

    Create form.py

---
+++ form.py
@@ -0,0 +1,18 @@
+## Alix Feinsod
+## Python form using template
+## Updated November 6, 2014
+## uses tryTemplate to take in data!!!
+
+from bottle import route, run, template, post, request
+
+@route('/formTest')
+def formTest():
+    output = template('tryTemplate')
+    return output
+    
+@route('/formTest', method='POST')
+def do_response():
+	userEmail = request.forms.get('useremail')
+	return "Processing your request. Results will be sent to '{0}' in a few hours or days.".format(userEmail)
+
+run(host='0.0.0.0', port=8080)
