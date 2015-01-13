6fb87ba5b323d11c7988454be49ae974fce5bb95
alfeinsod <alfeinsod@gmail.com>
2015-01-13 12:00:46 -0800

    Update form.py 1/13/15

--- form.py
+++ form.py
@@ -1,6 +1,6 @@
 ## Alix Feinsod
 ## Python form using template
-## Updated December 15, 2014
+## Updated January 13th, 2015
 ## uses tryTemplate to take in data!!!
 
 from bottle import route, run, template, post, request
@@ -15,45 +15,60 @@ from array import array
 
 from xastropy.stats import mcmc 
 from xastropy.igm.fN import data as fNdata
-#from xastropy.xutils import xdebug as xdb
+from xastropy.xutils import xdebug as xdb
 
 
 @route('/formTest')
 def formTest():
     return template('tryTemplate')
-    
+ 
 # Runs on click of "Submit data"
 @route('/formTest', method='POST')
 def do_response():
 	
 	dataSources = request.forms.getall('dataSource') # Grabs everything selected in a selection list
-	models = request.forms.getall('modelType') # List
-	modelType = models[0] # string
-	analyses = request.forms.getall('AnalysisOptions')
-	analysis = analyses[0] # string
-	outputs = request.forms.getall('OutputOptions') # List
-	#output = outputs[0]
+	if len(dataSources) == 0:
+		return "You must select at least one data source."
+	else:
+		models = request.forms.getall('modelType') # List
+		modelType = models[0] # string
+		analyses = request.forms.getall('AnalysisOptions')
+		analysis = analyses[0] # string
+		outputs = request.forms.getall('OutputOptions') # List
+		output = outputs[0] # string
+		
+		#import pdb
+		#pdb.set_trace()
+		#fNdata.tst_fn_data(outfil='tmp.png', data_list=dataSources)
+		#fNdata.tst_fn_data(outfil='tmp.png', data_list=models)
+		
+		useremail   = request.forms.get('useremail')
 
-	import pdb
-	pdb.set_trace()
-	fNdata.tst_fn_data(outfil='tmp.png', data_list=dataSources)
-
-    #fNdata.tst_fn_data(outfil='tmp.png', data_list=models)
+		time = mcmc.test()
+		msg = MIMEMultipart()
+		msg['Subject'] = "Email data"
+		msg['From'] = 'alfeinsod@gmail.com'
+		msg['To'] = useremail
+		msg.attach( MIMEText(str(time)))
 	
-	useremail   = request.forms.get('useremail')
-
-	time = mcmc.test()
-	msg     = str(time)
-
-   	smtp = smtplib.SMTP()
-   	smtp.connect('smtp.gmail.com', 587)
-   	smtp.starttls()
-   	smtp.ehlo()
+		files = []
+		#files.append('tmp.png')
 	
-    	smtp.sendmail('alfeinsod@gmail.com', useremail, msg)
-    	smtp.quit()
+    		for f in files:
+        		part = MIMEBase('application', "octet-stream")
+        		part.set_payload( open(f,"rb").read() )
+        		encoders.encode_base64(part)
+        		part.add_header('Content-Disposition', 'attachment; filename="{0}"'.format(os.path.basename(f)))
+        		msg.attach(part)
+   	
+   		smtp = smtplib.SMTP()
+   		smtp.connect('smtp.gmail.com', 587)
+   		smtp.starttls()
+   		smtp.ehlo()
+	
+    		smtp.sendmail('alfeinsod@gmail.com', useremail, msg.as_string())
+    		smtp.quit()
     	
-	return "Processing your request. Results will be sent to '{0}' in a few hours or days.".format(useremail)
-    #return template('tryTemplate2')
+		return "Processing your request. Results will be sent to '{0}' in a few hours or days.".format(useremail)
 
 run(host='0.0.0.0', port=8080)
