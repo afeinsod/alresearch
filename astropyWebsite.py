70668a7b89a21ae057e9a0d96f565b861edd68af
alfeinsod <alfeinsod@gmail.com>
2015-02-24 12:40:13 -0800

    update with todo list, questions

--- astropyWebsite.py
+++ astropyWebsite.py
@@ -1,6 +1,6 @@
 ## Alix Feinsod
 ## Python form using template
-## Updated January 27th, 2015
+## Updated February 24th, 2015
 ## uses astropyTemplate to take in data
 
 from bottle import route, run, template, post, request
@@ -12,7 +12,7 @@ from email.utils import COMMASPACE, formatdate
 from email import encoders
 from array import array
 
-
+##TODO import mcmc from igm.fN
 from xastropy.stats import mcmc 
 from xastropy.igm.fN import data as fNdata
 from xastropy.xutils import xdebug as xdb
@@ -35,6 +35,9 @@ def do_response():
 	dataSources = request.forms.getall('dataSource') # Grabs everything selected in a selection list
 	if len(dataSources) == 0:
 		return "You must select at least one data source."
+
+	## TODO: check if any user data sources. create boolean isUserSource = true if there is a user source, else false
+	
 	else:
 		models = request.forms.getall('modelType') # List
 		modelType = models[0] # string
@@ -43,8 +46,22 @@ def do_response():
 		outputs = request.forms.getall('OutputOptions') # List
 		output = outputs[0] # string
 		
-		import pdb
-		pdb.set_trace()
+
+		##TODO: 
+		##	- fN_data = set_fn_data(dataSources, extra_fNc=user input if isUserSource is true)
+		##		note: above includes making fn_data_from_fits-type function for ASCII
+		##	- fN_model = set_fn_model() Does this have any input? Does it need to be edited to take 
+		##		into account user model selection?
+		##	- rest is exact from run part of mcmc.py:
+		## 		- parm = set_pymc_var(fN_model)
+   		## 		- fN_model.param = np.array([iparm.value for iparm in parm])
+		## 		- run(fN_data, fN_model, parm)
+		##		- What about the output? how do we get the output of run and use it in the email?
+
+
+
+		#import pdb
+		#pdb.set_trace()
 		fNdata.tst_fn_data(outfil='tmp.png', data_list=dataSources)
 		#fNdata.tst_fn_data(outfil='tmp.png', data_list=models)
 		
@@ -70,7 +87,7 @@ def do_response():
    		smtp = smtplib.SMTP()
    		smtp.connect('smtp.gmail.com', 587)
    		smtp.starttls()
-   		stmp.login('xastropy@gmail.com','ucsc2015')
+   		smtp.login('xastropy@gmail.com','ucsc2015')
     		smtp.sendmail('xastropy@gmail.com', useremail, msg.as_string())
     		smtp.quit()
     	
