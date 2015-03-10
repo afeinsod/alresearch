bfcb01e36d5a63771c7cb43e91d6ea5e62701fff
alfeinsod <alfeinsod@gmail.com>
2015-03-10 12:36:10 -0700

    calls mcmc.py

--- astropyWebsite.py
+++ astropyWebsite.py
@@ -1,6 +1,6 @@
 ## Alix Feinsod
 ## Python form using template
-## Updated February 24th, 2015
+## Updated March 5th, 2015
 ## uses astropyTemplate to take in data
 
 from bottle import route, run, template, post, request
@@ -12,8 +12,8 @@ from email.utils import COMMASPACE, formatdate
 from email import encoders
 from array import array
 
-##TODO import mcmc from igm.fN
-from xastropy.stats import mcmc 
+
+from xastropy.igm.fN import mcmc 
 from xastropy.igm.fN import data as fNdata
 from xastropy.xutils import xdebug as xdb
 
@@ -35,37 +35,37 @@ def do_response():
 	dataSources = request.forms.getall('dataSource') # Grabs everything selected in a selection list
 	if len(dataSources) == 0:
 		return "You must select at least one data source."
-
-	## TODO: check if any user data sources. create boolean isUserSource = true if there is a user source, else false
 	
 	else:
+		builtInSources = ['OPB07', 'OPW12', 'OPW13', 'K05', 'K13R13', 'N12']
+		extraSources = []
+		sources = []
+		for src in dataSources:
+			if src not in builtInSources:
+				src = os.path.abspath(src)
+				extraSources.append(src)
+				sources.append(src)
+			else:
+				sources.append(src)
+				
 		models = request.forms.getall('modelType') # List
 		modelType = models[0] # string
 		analyses = request.forms.getall('AnalysisOptions')
 		analysis = analyses[0] # string
 		outputs = request.forms.getall('OutputOptions') # List
 		output = outputs[0] # string
-		
-
-		##TODO: 
-		##	- fN_data = set_fn_data(dataSources, extra_fNc=user input if isUserSource is true)
-		##		note: above includes making fn_data_from_fits-type function for ASCII
-		##	- fN_model = set_fn_model() Does this have any input? Does it need to be edited to take 
-		##		into account user model selection?
-		##	- rest is exact from run part of mcmc.py:
-		## 		- parm = set_pymc_var(fN_model)
-   		## 		- fN_model.param = np.array([iparm.value for iparm in parm])
-		## 		- run(fN_data, fN_model, parm)
-		##		- What about the output? how do we get the output of run and use it in the email?
-
-
-
-		#import pdb
-		#pdb.set_trace()
-		fNdata.tst_fn_data(outfil='tmp.png', data_list=dataSources)
-		#fNdata.tst_fn_data(outfil='tmp.png', data_list=models)
-		
 		useremail   = request.forms.get('useremail')
+
+		fN_data = mcmc.set_fn_data(sources, extraSources)
+		fN_model = mcmc.set_fn_model() 
+		parm = mcmc.set_pymc_var(fN_model)
+		fN_model.param = mcmc.np.array([iparm.value for iparm in parm])
+		mcmc.run(fN_data, fN_model, parm, useremail)
+		
+		import pdb
+		pdb.set_trace()
+		#fNdata.tst_fn_data(outfil='tmp.png', data_list=dataSources)
+		#fNdata.tst_fn_data(outfil='tmp.png', data_list=models)
 
 		time = mcmc.test()
 		msg = MIMEMultipart()
