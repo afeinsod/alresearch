803af56e5d3cd854c68aacd9b619a3fe0d4210e8
alfeinsod <alfeinsod@gmail.com>
2015-05-11 12:01:46 -0700

    current error: all types in unput file are str. Must cast to float.

--- igmfn.py
+++ igmfn.py
@@ -6,9 +6,8 @@
 from bottle import route, run, template, post, request
 import smtplib, os
 import commands
-import cgi, cgitb
 from bottle import static_file
-
+import subprocess
 
 @route('/static/<filename>')
 def server_static(filename):
@@ -21,7 +20,7 @@ def form():
 # Runs on click of "Submit data"
 @route('/astropy', method='POST')
 def do_response():
-	
+	useremail   = request.forms.get('useremail')
 	dataSources = request.forms.getall('dataSource') # Grabs everything selected in a selection list
 	if len(dataSources) == 0:
 		return "You must select at least one data source."
@@ -33,13 +32,43 @@ def do_response():
 		for src in dataSources:
 			if src not in builtInSources:
 				#we have a user upload
-				#first, save it
-				form = cgi.FieldStorage()
-				filedata = form['upload']
-				if filedata.file: # field really is an upload
-    					with file(src + '.txt', 'w') as outfile:
-        					outfile.write(filedata.file.read())
-				#second, add it to the list 
+				#first, make sure the format is correct
+				upload = request.files.get('upload')
+				flag = True
+				f=upload.file.read()
+				import pdb
+				pdb.set_trace()
+				lines = f.split("\n")
+				
+				firstline=lines[0].split(" ")
+				 
+				if len(firstline) != 2:
+					flag = False
+				for i in firstline:
+					if type(i) is not float:
+						flag = False
+						
+				for i in range(1, len(lines) -1):
+					line = lines[i].split(" ") 
+					if len(line) != 4:
+						flag = False
+					for a in line:
+						if type(a) is not float:
+							flag = False
+				
+				if flag is False:
+					return "Wrong file format."
+					
+				#second, save it
+				name, ext = os.path.splitext(upload.filename)
+				save_path = "/tmp/{email}".format(email=useremail)
+        			if not os.path.exists(save_path):
+           				os.makedirs(save_path)
+
+        			file_path = "{path}/{file}".format(path=save_path, file=upload.filename)
+				with open(file_path, 'w') as open_file:
+        				open_file.write(upload.file.read())
+				#third, add it to the list 
 				# not sure if this is necessary: src = os.path.abspath(src)
 				extraSources.append(src)
 				sources.append(src)
@@ -52,18 +81,20 @@ def do_response():
 		analysis = analyses[0] # string
 		outputs = request.forms.getall('OutputOptions') # List
 		output = outputs[0] # string
-		useremail   = request.forms.get('useremail')
+		
     ##Sends this info to profx machine by mkdir, generating an SCP request for any user uploads, executes a 
     ##python call on profx to run in the background on (some amount) of processors. 
-		if length(extraSources) != 0: 
+		if (len(extraSources) != 0): 
 			#makes directory in afeinsod
 			HOST = 'afeinsod@profx'
-			COMMAND = 'mkdir' + useremail
+			COMMAND = 'mkdir ' + useremail
 			ssh = subprocess.call(["ssh", "-C", "%s" % HOST, COMMAND])
 			#copies all user uploads to that directory
 			for src in extraSources:
 				p=subprocess.call(['scp', src, 'profx:/Users/afeinsod/' + useremail])
-		COMMAND = 'python profx.py ' + sources + extraSources + modelType + analysis + output + useremail 
+		COMMAND = 'python profx.py ' + modelType + analysis + output + useremail 
+		for src in sources:
+			COMMAND = COMMAND + src
 		process=subprocess.Popen(["ssh", "-C", "%s" % HOST, COMMAND])
 				
 		return "Processing your request. Results will be sent to '{0}' in a few hours or days.".format(useremail)
