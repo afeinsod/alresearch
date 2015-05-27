d11dcf012169719ef467cedd3ec053b233f6a42e
alfeinsod <alfeinsod@gmail.com>
2015-05-26 22:04:09 -0700

    dropbox really fixed

--- profx.py
+++ profx.py
@@ -1,9 +1,9 @@
 ## Alix Feinsod
 ## Process of igmfn, for execution on profx comp
 ## Created April 22nd, 2015
+## Last edited May 26th, 2015
 ## runs MCMC and emails results
 
-from bottle import route, run, template, post, request
 import smtplib, os
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
@@ -13,22 +13,21 @@ from email import encoders
 from array import array
 from email.mime.application import MIMEApplication
 
-from xastropy.igm.fN import mcmc 
-from xastropy.igm.fN import data as fNdata
-from xastropy.xutils import xdebug as xdb
+#from xastropy.igm.fN import mcmc 
+#from xastropy.igm.fN import data as fNdata
+#from xastropy.xutils import xdebug as xdb
 
-from bottle import static_file
 import shutil
 
 def runmcmcemail(modelType, useremail, sources, extraSources):
 		
-		if modelType is 'Akio Model':
-			mcmc.mcmc_main(useremail, sources, extraSources, 1)
-		else:
-			mcmc.mcmc_main(useremail, sources, extraSources)
+		#if modelType is 'Akio Model':
+		#	mcmc.mcmc_main(useremail, sources, extraSources, 1)
+		#else:
+		#	mcmc.mcmc_main(useremail, sources, extraSources)
 	
-		files = os.listdir('C:/Xastropy Output Files/' + useremail)
-
+		#files = os.listdir('C:/Xastropy Output Files/' + useremail)
+		files = ['testfile.txt']
 		path = '/Users/afeinsod/Dropbox/Output/'
 		if os.path.isdir(path + useremail):  
             		path = path +  useremail
@@ -40,13 +39,13 @@ def runmcmcemail(modelType, useremail, sources, extraSources):
 			oldpath = os.path.abspath(f)
         		shutil.copy2(oldpath, path)
 
-		dropboxLink = 'https://www.dropbox.com/sh/lokufndo4gc7mbz/AACFRsF5qewnni8f4AYoUg8ka?dl=0'
+		dropboxLink = 'https://www.dropbox.com/sh/mjtc95fb8g4cdbb/AADAHSXgUvg3HzPzxy-MKezCa?dl=0'
 
 		msg = MIMEMultipart()
 		msg['Subject'] = "IGMFN"
 		msg['From'] = 'xastropy@aol.com'
 		msg['To'] = useremail
-		msg.attach( MIMEText(str('Hello, \nYou can access your results from igmfn.ucolick.org here: ' + dropboxLink + ' in the folder ladeled with your email. \nBest, \n UCO/Lick')))
+		msg.attach( MIMEText(str('Hello, \nYou can access your results from igmfn.ucolick.org here in the folder ladeled with your email: ' + dropboxLink + '\nBest, \nUCO/Lick')))
 
    		smtp = smtplib.SMTP()
    		smtp.connect('smtp.aol.com', 587)
@@ -63,7 +62,7 @@ if __name__ == '__main__':
 
     	# PARSE 
     	parser = argparse.ArgumentParser(description='profx code for igmfn')
-    	parser.add_argument("infile", type=string, help="Name of input ascii file")
+    	parser.add_argument("infile", type=str, help="Name of input ascii file")
     	args = parser.parse_args()
 	infile=args.infile
 	
@@ -73,11 +72,15 @@ if __name__ == '__main__':
         #format: firstline = modelType, second = useremail, third = sources split by spaces,
         #fourth = extrasources split by spaces
         
-        f=infile.read()
-	lines = f.split("\n")
+        f=open(infile, 'r')
+	filein=f.read()
+	lines = filein.split("\n")
 				
 	modelType=lines[0].split(" ")
 	useremail=lines[1]
 	sources=lines[2].split(" ")
-	extraSources=lines[3].split(" ")
+	if len(lines) >3:
+		extraSources=lines[3].split(" ")
+	else:
+		extraSources=[]
 	runmcmcemail(modelType, useremail, sources, extraSources)
